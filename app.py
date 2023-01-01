df2bd1bd2fd6c39709923dcefe5f5ec5c6dde351
zaelani23 <mohamadzaelani09@gmail.com>
2023-01-01 11:26:03 +0700

    revisi kode

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import datetime
 import matplotlib.pyplot as plt
 from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 
 st.set_page_config(page_title='Peramalan Harga Beras')
 st.write("""
 # Hasil Peramalan Harga Beras Januari 2022 - Juni 2022 Menggunakan Model GRU
 """)
-tab1, tab2 = st.tabs(["Forecasting a few days", "Forecasting one day"])
+tab1, tab2, tab3 = st.tabs(["Data", "Forecasting a few days", "Forecasting one day"])
 my_model_params = [
     [32, 7, 100],
     [32, 30, 100],
@@ -23,17 +23,38 @@ my_model_params = [
 ]
 
 with tab1:
+    st.info("Model pada aplikasi ini dilatih menggunakan data harga beras IR-64 I harian antara 1 Januari 2016 - 31 Desember 2021 yang diambil dari website PT FoodStation")
+    dataset = pd.read_csv("Data Beras IR-64 I.csv")
+    dataset.insert(loc = 0,
+            column = 'No',
+            value = range(1, len(dataset) + 1))
+    gb = GridOptionsBuilder.from_dataframe(dataset)
+    gb.configure_pagination(
+        paginationAutoPageSize=False, 
+        paginationPageSize=10
+    )
+    gridOptions = gb.build()
+    AgGrid(dataset, gridOptions=gridOptions)
+
+    fig, ax = plt.subplots()
+    ax.plot(dataset['IR-64 I'])
+    ax.set_title("Visualisasi Harga Beras IR-64 I 2016 - 2021")
+    ax.set_xlabel("Days")
+    ax.set_ylabel("Price")
+    st.pyplot(fig)
+
+with tab2:
     model = st.radio(
         "Select model",
         ('Model 1', 'Model 2', 'Model 3', 'Model 4',
          'Model 5', 'Model 6', 'Model 7', 'Model 8')
     )
-
+    
     filename = 'Data Prediksi Beras 2022 Skenario {}.csv'.format(model.split()[1])
     df = pd.read_csv(filename)
     df.insert(loc = 0,
             column = 'No',
-            value = range(1, 182))
+            value = range(1, 365))
     tanggal = df['Tanggal'].values
 
     params = my_model_params[int(model.split()[1]) - 1]
@@ -44,8 +65,8 @@ with tab1:
 
     number = st.slider(
         "Pick number of days to forecast", 
-        min_value = 1, max_value = 181 , 
-        value=(1, 181)
+        min_value = 1, max_value = 364 , 
+        value=(1, 364)
     )
     start_date = datetime.datetime.strptime(
         tanggal[number[0] - 1], 
@@ -74,20 +95,37 @@ with tab1:
         new_df['IR-64 I Predictions Price'].values,
         squared=False
     )
+    mape = mean_absolute_percentage_error(
+        new_df['IR-64 I Actual Price'].values, 
+        new_df['IR-64 I Predictions Price'].values,
+    )
+    if (mape < 0.1):
+        justifikasi = "sangat bagus"
+    elif (mape >= 0.1 and mape < 0.2):
+        justifikasi = "bagus"
+    elif (mape >= 0.2 and mape < 0.5):
+        justifikasi = "cukup bagus"
+    else:
+        justifikasi = "kurang bagus"
+
     st.info("""
     Mean Squared Error : **{}**\n
-    Root Mean Squared Error : **{}**
-    """.format(mse, mse_without_squared))
-    st.write("""
-    Visualisasi Harga Beras Actual dan Prediksi
-    """)
+    Root Mean Squared Error : **{}**\n
+    Mean Absolute Precentage Error : **{}**\n
+    \n
+    Berdasarkan nilai metrik diatas, maka hasil dari peramalan model dapat dikatakan **{}**.
+    """.format(mse, mse_without_squared, mape, justifikasi))
+
     arr = new_df[['IR-64 I Actual Price', 'IR-64 I Predictions Price']]
     fig, ax = plt.subplots()
     ax.plot(arr)
     ax.legend(['Actual Price', 'Predictions Price'])
+    ax.set_title("Visualisasi Harga Beras Actual dan Prediksi")
+    ax.set_xlabel("Days")
+    ax.set_ylabel("Price")
     st.pyplot(fig)
 
-with tab2:
+with tab3:
     d = st.date_input(
         "Select date to forecast",
         datetime.date(2022, 1, 1),
